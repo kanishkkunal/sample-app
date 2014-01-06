902d0d2a6b627ef6257d818f6776bf7dc44e7604
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-01-06 18:51:51 +0530

    Cusom Font on Windows Phone Sample

---
+++ SampleApp/SamplePages/CustomFont.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace SampleApp.SamplePages
+{
+    public partial class CustomFont : PhoneApplicationPage
+    {
+        public CustomFont()
+        {
+            InitializeComponent();
+            //Font Source: http://www.dafont.com/elegant-ink.font
+            //See http://blog.superdevresources.com/ for more free fonts
+            appTitle.FontFamily = new FontFamily("/Assets/Fonts/elegant_ink.ttf#Elegant Ink");
+            TodosListToday.ItemsSource = new String[] { "Car Wash", "Buy Groceries", "Complete Assignment" };
+            TodosListLater.ItemsSource = new String[] { "Buy Shoes", "Study for Exams", "Practice for tennis match" };
+        }
+    }
+}
